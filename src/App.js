31d2e49a351d544984eb2843febff450943a4e32
Avinash Jha <avinash.jha@easebuzz.in>
2024-01-08 11:39:44 +0530

    added speed insights comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles/styles.css";
 import logoImage from "./assets/images/Brand_Logo.png";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <span className="light-text">made remotely by</span>{" "}
         <span className="dark-text">Suchit & Avinash</span>
       </p>
+      {/* <SpeedInsights /> */}
     </div>
   );
 }
